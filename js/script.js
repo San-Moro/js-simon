687aae1802ddbf84d72347500418c917eea5624b
San-Moro <valentinacortese.dsg@gmail.com>
2022-10-19 16:03:15 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/**Descrizione:
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri spariscono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
+
+/**
+ * Crea Array simonNumbers
+ * Crea Array userNumbers
+ * Genera 5 numeri casuali 
+ * salvare i 5 numeri random nell'Array simonNumbers
+ * mostra a schermo i numeri simonNumbers per 30 sec
+ *  crea timer setTimeout che dopo 30 minuti
+ *      nasconde i numeri simonNumbers
+ *      chiede for 5 volte i numeri che erano a schermo
+ * confronta simonNumbers e userNumbers
+ * Crea Array result
+ * SE tra questi due Array ci sono numeri uguali ===
+ *      questi .this vengono salvati nell'Array result
+ * Else 
+ *      niente, non vengono salvati
+ * Mostra a schermo il risultato di Array result
+ */     
