459d0a351b8c76d27752c1a9963d9580d2295989
San-Moro <valentinacortese.dsg@gmail.com>
2022-10-31 15:59:07 +0100

    exercise's correction

--- js/script.js
+++ js/script.js
@@ -3,38 +3,41 @@ Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
 Dopo 30 secondi i numeri spariscono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
 
-/**
- * [*] Crea Array simonNumbers
- * [] Crea Array userNumbers
- * [*] Genera 5 numeri casuali in un range da 1 a 100 ==> function generateNumbersRndm
- * [*] salvare i 5 numeri random nell'Array simonNumbers
- * [*] mostra a schermo i numeri simonNumbers per 30 sec
- *  [] crea timer setTimeout che dopo 30 secondi
- *      [] nasconde i numeri simonNumbers
- *      [] chiede for 5 volte i numeri che erano a schermo
- * [] Confronta simonNumbers e userNumbers
- * [] Crea Array result
- * [] SE tra questi due Array ci sono numeri uguali ===
- *      [] questi .this vengono salvati nell'Array result
- * [] Else 
- *      [] niente, non vengono salvati
- * [] Mostra a schermo il risultato di Array result
-*/
 
-// simonNumbers è l'Array di 5 numeri random in un range da 1 a 100
+
+// generatedNumbers è l'Array di 5 numeri random generati in un range da 1 a 100
 const simonNumbers = 5;
 const generatedNumbers = generateNumbersRndm(simonNumbers);
 console.log(generatedNumbers);
 
+const numberScreen = document.getElementById("numbers-container");
+
 // mostra a schermo i numeri random generati
-const numberScreen = document.querySelector("h1");
 numberScreen.innerHTML = generatedNumbers;
 
-// userNumbers è l'Array di 5 numeri inseriti dall'utente tramite la funzione toAskNumber
-const userNumbers = [];
+// Alla scadenza del tempo nascondere i numeri
+setTimeout(function() {
+    numberScreen.innerHTML = "";
+}, 3000);
+
 
 // setTimeout che dopo 30 secondi chiede all'utente i numeri che sono appena stati nascosti
-setTimeout(toAskNumber, 3000);
+// userNumbers è l'Array di 5 numeri inseriti dall'utente tramite la funzione toAskNumber
+
+setTimeout(function() {
+    const userNumbers = toAskNumber(simonNumbers);
+    console.log(userNumbers);
+    // confrontare i due array per poi salvare i numeri indovinati
+    const numbersGuessed = compareNumbers(generatedNumbers, userNumbers);
+    console.log(numbersGuessed);
+    // stampare Output
+    printResult(numbersGuessed);
+
+}, 4000);
+
+
+
+
 
 // FUNCTIONS
 
@@ -62,17 +65,51 @@ function generateNumbersRndm(howManyNumbers) {
 }
 
 /**toAskNumber
- * Description: funzione per chiedere il numero all'utente 
- * @param {number} 
- * @returns {Array} array di numeri 
+ * Description: funzione per chiedere i numeri all'utente e salva in un array
+ * @param {number} numbersQty- la quantità di tentativi 
+ * @returns {Array} array di numeri inseriti dall'utente
 */
 
-function toAskNumber() {
-    let userAnswer = parseInt(prompt("Inserisci i numeri che hai visto precedentemente"));
+function toAskNumber(numbersQty) {
+    const userArray = [];
     // chiedi i numeri finchè userAnswer.length < simonNumbers.length
-    while(userAnswer.length < simonNumbers.length) {
+    for (let i = 0; i < numbersQty; i++) {
+        const userAnswer = parseInt(prompt("Inserisci un numero che hai visto precedentemente"));
+        userArray.push(userAnswer);
     }
+    return userArray;
 }
+
+/**
+ * Description : confronta i due array e restituisce l'array con gli elementi in comune fra i due
+ * @param {Array} firstArray
+ * @param {Array} secondArray
+ * @returns {Array} Array di numeri in comune
+ */
+function compareNumbers(firstArray, secondArray) {
+    const result = [];
+    //ciclare il primo array
+    for (let i = 0; i<firstArray.length; i++) {
+        const thisElement = firstArray[i];
+        //Se questo elemento è incluso nel secondo array
+        if (secondArray.includes(thisElement)) {
+            //lo pusho nell'array del risultato
+            result.push(thisElement);
+        }
+    }
+    return result;
+}
+
+
+/**
+ * Description: stampa i numeri indovinati e la loro quantità
+ * @param {any} resultArray è l'array di numeri da stampare
+ */
+function printResult(resultArray) {
+    document.getElementById("result").innerHTML = `Hai indovinato ${resultArray.length} numeri ${resultArray}`;
+
+}
+
 
 
 
